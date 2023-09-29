e426b63653300ee2a877aae107a0b0fa429de671
Afonso <ferrer.afonso@gmail.com>
2023-09-29 11:10:30 -0300

    [chore] Added file with Houses's colors

---
+++ src/color.ts
@@ -0,0 +1,6 @@
+export const colors = {
+  gryffindor: { primaryColor: "660000", secondaryColor: "e09c09" },
+  slythering: { primaryColor: "2f751c", secondaryColor: "cccccc" },
+  ravenclaw: { primaryColor: "1a3956", secondaryColor: "8e501d" },
+  hufflepuf: { primaryColor: "1f1e19", secondaryColor: "ff9d0b" },
+}
