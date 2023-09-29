250d49982886f5418ba8cefb5f9ab8b8f543dc1f
Afonso <ferrer.afonso@gmail.com>
2023-09-29 11:14:16 -0300

    [chore] Created type validator for character
    - To help with parsing an API response
    - Wand is still missing, going to fix this later

--- src/types/character.ts
+++ src/types/character.ts
@@ -9,6 +9,21 @@ import {
   Patronus,
 } from "./base";
 
+import {
+  isString,
+  isNumber,
+  isBoolean,
+  isStringArray,
+  isOfGender,
+  isOfSpecies,
+  isOfPatronus,
+  isOfHouse,
+  isOfAncestry,
+  isOfEyeColour,
+  isOfHairColour,
+  optional,
+} from "./type-guards";
+
 export type Character = {
   id: string;
   name: string;
@@ -30,4 +45,30 @@ export type Character = {
   alternateActors?: string[];
   alive: boolean;
   image: string;
+};
+
+export const CharacterValidator: Record<
+  keyof Character,
+  (property: unknown) => boolean
+> = {
+  id: isString,
+  name: isString,
+  dateOfBirth: (data) => optional(data, isString),
+  yearOfBirth: (data) => optional(data, isNumber),
+  wizard: isBoolean,
+  hogwartsStudent: isBoolean,
+  hogwartsStaff: isBoolean,
+  actor:  (data) => optional(data, isString),
+  alive: isBoolean,
+  image: isString,
+  alternateNames:  (data) => optional(data, isStringArray),
+  alternateActors:  (data) => optional(data, isStringArray),
+  species: isOfSpecies,
+  gender: isOfGender,
+  house:  (data) => optional(data, isOfHouse),
+  ancestry:  (data) => optional(data, isOfAncestry),
+  eyeColour:  (data) => optional(data, isOfEyeColour),
+  hairColour:  (data) => optional(data, isOfHairColour),
+  wand: () => true, // @todo fix this
+  patronus:  (data) => optional(data, isOfPatronus),
 };
