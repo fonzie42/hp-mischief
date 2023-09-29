94c4559a2b581f9c7c605055a580cf2765d17874
Afonso <ferrer.afonso@gmail.com>
2023-09-29 11:11:03 -0300

    [chore] Added consts with all possible values for characters

---
+++ src/types/CONSTANTS.ts
@@ -0,0 +1,119 @@
+export const ALL_SPECIES = [
+  "acromantula",
+  "cat",
+  "centaur",
+  "dragon",
+  "ghost",
+  "giant",
+  "goblin",
+  "half-giant",
+  "half-human",
+  "hippogriff",
+  "house-elf",
+  "human",
+  "owl",
+  "poltergeist",
+  "three-headed dog",
+  "vampire",
+  "werewolf",
+] as const
+
+
+export const ALL_GENDERS = ["male", "female"] as const
+
+export const ALL_HOUSES = [
+  "Gryffindor",
+  "Hufflepuff",
+  "Ravenclaw",
+  "Slytherin",
+] as const
+
+export const ALL_ANCESTRIES = [
+  "half-blood",
+  "half-veela",
+  "muggle",
+  "muggleborn",
+  "pure-blood",
+  "quarter-veela",
+  "squib",
+] as const
+
+export const ALL_EYE_COLOUR = [
+  "amber",
+  "black",
+  "blue",
+  "brown",
+  "dark",
+  "green",
+  "grey",
+  "hazel",
+  "orange",
+  "pale, silvery",
+  "red",
+  "white",
+  "yellow",
+  "yellowish",
+] as const
+
+export const ALL_HAIR_COLOUR = [
+  "bald",
+  "black",
+  "blond",
+  "blonde",
+  "brown",
+  "dark",
+  "dull",
+  "ginger",
+  "grey",
+  "red",
+  "sandy",
+  "silver",
+  "tawny",
+  "white",
+] as const
+
+export const ALL_WAND_WOOD = [
+  "ash",
+  "birch",
+  "cedar",
+  "cherry",
+  "chestnut",
+  "cypress",
+  "elm",
+  "fir",
+  "hawthorn",
+  "holly",
+  "hornbeam",
+  "larch",
+  "mahogany",
+  "oak",
+  "vine",
+  "walnut",
+  "willow",
+  "yew",
+] as const
+
+export const ALL_WAND_CORE = [
+  "dragon heartstring",
+  "phoenix feather",
+  "unicorn hair",
+  "unicorn tail-hair",
+] as const
+
+export const ALL_PATRONUS = [
+  "boar",
+  "doe",
+  "goat",
+  "hare",
+  "horse",
+  "Jack Russell terrier",
+  "lynx",
+  "Non-Corporeal",
+  "otter",
+  "persian cat",
+  "stag",
+  "swan",
+  "tabby cat",
+  "weasel",
+  "wolf",
+] as const
