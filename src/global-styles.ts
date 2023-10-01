2b9e2ffd9990d711e20c5281472e687997b5e5d9
Afonso <ferrer.afonso@gmail.com>
2023-10-01 18:28:50 -0300

    [chore] Added Globalstyles and removed css files

---
+++ src/global-styles.ts
@@ -0,0 +1,16 @@
+import { createGlobalStyle } from 'styled-components'
+
+
+export const GlobalStyle = createGlobalStyle`
+body {
+
+  width: 100%;
+} 
+html {
+  width: 100%;
+
+} 
+#root { 
+  width: 100%;
+}
+`
--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
 
-import 'react-loading-skeleton/dist/skeleton.css'
-
-ReactDOM.createRoot(document.getElementById('root')!).render(
+import "react-loading-skeleton/dist/skeleton.css";
+import { GlobalStyle } from "./global-styles.ts";
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+      <GlobalStyle />
+      <App />
+  </React.StrictMode>
+);
