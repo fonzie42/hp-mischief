f92e2802353b5aaa1d5e8ebc93c1191f0138b422
Afonso <ferrer.afonso@gmail.com>
2023-10-01 19:58:40 -0300

    [feat] refinement for CharacterFolder animation

--- src/components/character-folder/character-folder.styled.ts
+++ src/components/character-folder/character-folder.styled.ts
@@ -1,30 +1,24 @@
 import { mediaQueries } from "@/styles"
-import styled, {css, keyframes} from "styled-components"
-import {
-  NavLink,
-} from "react-router-dom"
+import styled, { css, keyframes } from "styled-components"
+import { NavLink } from "react-router-dom"
 
 const reveal = keyframes`
-from {transform: skew(10deg, -10deg) translate3d(100%,100px,0)}
-to {transform: skew(10deg, -10deg) translate3d(0,0,0)}
+  from {
+    transform: skew(10deg, -10deg) translate3d(100%,100px,0);
+  }
+  
+  to {
+    transform: skew(10deg, -10deg) translate3d(0,0,0);
+  }
 `
 
-export const Wrapper = styled(NavLink)<{animationDelay: string | null}>`
+export const Wrapper = styled(NavLink)<{ animationDelay: string | null }>`
   background: #c8ad80;
   padding: 8px 20px;
   background-image: url("https://images.unsplash.com/photo-1615800098746-73af8261e3df?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1888&q=80");
   background-blend-mode: overlay;
   border: 2px solid black;
   border-radius: 10px 10px 0 0;
-
-  ${({ animationDelay }) => 
-  css`
-  transform: skew(10deg, -10deg) translate3d(100%,100px,0);
-  animation: ${reveal} .4s;
-  animation-delay: ${animationDelay};
-  `
-  }
-
   height: 110px;
   margin-bottom: -50px;
   font-family: "Jim Nightshade", cursive;
@@ -34,32 +28,40 @@ export const Wrapper = styled(NavLink)<{animationDelay: string | null}>`
   margin-left: 2em;
   margin-right: -2em;
   display: flex;
-
+   box-shadow: 16px -8px 14px 5px rgba(0, 0, 0, 0.15);
+  margin-left: 2em;
+  transform: skew(10deg, -10deg);
+  
+  ${({ animationDelay }) =>
+    animationDelay &&
+    css`
+      animation: ${reveal} 0.4s;
+      animation-delay: ${animationDelay};
+      
+    `}
 
   &:hover {
-    transform:  translateY(-10px) skew(0deg, 0deg);
+    transform: translateY(-10px) skew(0deg, 0deg);
     margin-top: -20px;
   }
-
-  transform: skew(10deg, -10deg);
-  box-shadow: 16px -8px 14px 5px rgba(0, 0, 0, 0.15);
-  margin-left: 2em;
 
   ${mediaQueries.mobileTiny`
     margin-right: -30px;
     font-size: 32px;
     height: 80px;
     width: 290px;
-    padding: 0;
-    
+    padding: 0
     margin-bottom: -40px;
+
     &:hover {
       transform: skew(0deg, 0deg);
     }
   `}
-  
+
   ${mediaQueries.tablet`
-     font-size: 44px;
+    font-size: 44px;
+    height: 170px;
+    margin-bottom: -120px;
   `}
 `
 
@@ -69,7 +71,7 @@ export const FoldersWrapper = styled.div`
   overflow: hidden;
   display: flex;
   flex-direction: column;
-  
+
   ${mediaQueries.mobileTiny`
   
 padding: 64px 0;
