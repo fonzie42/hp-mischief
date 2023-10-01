0945a626dc6f44ff784710cd12f74b824053fdc4
Afonso <ferrer.afonso@gmail.com>
2023-10-01 18:31:21 -0300

    [feat] Added router
    - Basic route
    - Character info route

--- src/main.tsx
+++ src/main.tsx
@@ -4,9 +4,32 @@ import App from "./App.tsx";
 
 import "react-loading-skeleton/dist/skeleton.css";
 import { GlobalStyle } from "./global-styles.ts";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { Error } from "./routes/error.tsx";
+import { Character, characterLoader } from "./routes/character.tsx";
+
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <Error />,
+  },
+  {
+    path: "character/:characterId",
+    element: <Character />,
+    loader: characterLoader,
+  },
+]);
+
+const queryClient = new QueryClient();
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
+    <QueryClientProvider client={queryClient}>
       <GlobalStyle />
-      <App />
+      <RouterProvider router={router} />
+    </QueryClientProvider>
   </React.StrictMode>
 );
---
+++ src/routes/character.tsx
@@ -0,0 +1,58 @@
+import {  getCharacterById } from "@/api/base";
+import { CharacterInfo } from "@/components/character-info";
+import { useCharacterStore } from "@/store/store";
+import { Form, useLoaderData } from "react-router-dom";
+ import {
+  useQuery,
+  useMutation,
+  useQueryClient,
+  QueryClient,
+  QueryClientProvider,
+} from "@tanstack/react-query";
+import { useEffect } from "react";
+
+export const characterLoader = ({ params }) => { //@todo fix type for router
+  console.log({ params })  
+  return params
+}
+
+export const Character = () => {
+  const { characterId } = useLoaderData(); //@todo fix type for router
+  const characters = useCharacterStore(state => state.characters)
+  const updateCharacter = useCharacterStore(state => state.updateCharacter)
+
+  const selectedCharacterFromState = characters.find(item => item.id === characterId)
+  const enabled = !selectedCharacterFromState
+  
+  const { data, isError, isLoading: isLoadingQuery } = useQuery({
+    queryKey: [characterId], 
+    queryFn: () => getCharacterById(characterId as string),
+    enabled: enabled
+  })
+
+  const selectedCharacter = selectedCharacterFromState ?? data // to avoid "not found" false positive
+
+  useEffect(() => {
+    data && updateCharacter(data)
+  }, [data])
+
+  const isLoading = isLoadingQuery && enabled;
+
+  if (isLoading) {
+    console.log('loading')
+    return <div>Loading</div>
+  }
+  
+  if (isError) {
+    return <div>Sorry, something went wrong</div>
+  }
+  
+  if (!selectedCharacter) {
+    console.log('not found')
+    return <div> No files were found. </div>
+  }
+  
+
+  return <CharacterInfo character={selectedCharacter} />
+
+}
