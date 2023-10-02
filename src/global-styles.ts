54f8a302ea271907aa42c7b71f43ea19198227d7
Afonso Ferrer <afonso.ferrer@jackinthebox.com>
2023-10-02 15:58:14 -0300

    [refactor] Updated global styles
    - Better background handling with no-repeat and position fixed
    - Avoid horizontal scrollbar with overflow-x hidden. This works because all components that are being rendered that create the hroizontal scrollbar are because of transforms

--- src/global-styles.ts
+++ src/global-styles.ts
@@ -6,11 +6,14 @@ body {
   width: 100%;
   font-family: "Special Elite", "Jim Nightshade", cursive;
   background-image: url(https://images.unsplash.com/photo-1547756536-cde3673fa2e5?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2141&q=80);
-
+  overflow-x: hidden;
+  overflow-y: auto;
+  background-repeat: no-repeat;
+  background-attachment: fixed;
 
   &:before {
     content: "";
-    position: absolute;
+    position: fixed;
     top: 0;
     left: 0;
     width: 100%;
@@ -27,5 +30,8 @@ html {
 } 
 #root { 
   width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-end;
 }
 `
