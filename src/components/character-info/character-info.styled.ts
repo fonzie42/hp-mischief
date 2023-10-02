363c2a7393912003495e88223e9607e95a66f1f0
Afonso Ferrer <afonso.ferrer@jackinthebox.com>
2023-10-02 15:57:43 -0300

    [feat] Added Wand information for characters

--- src/components/character-info/character-info.styled.ts
+++ src/components/character-info/character-info.styled.ts
@@ -1,5 +1,5 @@
 import { mediaQueries } from "@/styles"
-import styled, { keyframes } from "styled-components"
+import styled, { css, keyframes } from "styled-components"
 
 const reveal = keyframes`
 
@@ -11,12 +11,11 @@ export const Wrapper = styled.div`
   background: #c8ad80;
   background-image: url("https://images.unsplash.com/photo-1615800098746-73af8261e3df?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1888&q=80");
   background-blend-mode: overlay;
-  margin: 0;
+  margin: auto;
+  margin-top: 32px;
   text-align: center;
   box-shadow: inset 0px 0px 16px 7px #e4b972;
-  animation: ${reveal} 1s forwards cubic-bezier(.25, .25, .25, 1.25);
-
-  
+  animation: ${reveal} 1s forwards cubic-bezier(0.25, 0.25, 0.25, 1.25);
   padding: 32px;
 
   ${mediaQueries.mobileTiny`
@@ -26,11 +25,10 @@ export const Wrapper = styled.div`
 
   ${mediaQueries.tablet`
     padding: 32px;
-    margin: 8px;
   `}
 `
 
-export const Information = styled.div`
+export const Information = styled.div<{ $noMarginTop?: boolean }>`
   font-family: "Special Elite", cursive;
   letter-spacing: 2px;
   margin: 0;
@@ -38,11 +36,19 @@ export const Information = styled.div`
   flex-direction: column;
   grid-template-columns: 140px 1fr;
   text-align: right;
-  
+
   ${mediaQueries.tablet`
-    margin-top: 48px;
     grid-template-columns: 140px 1fr 140px 1fr;
   `};
+
+  ${({ $noMarginTop }) =>
+    $noMarginTop
+      ? css`
+          margin-top: 0;
+        `
+      : css`
+          margin-top: 48px;
+        `}
 `
 export const Term = styled.div`
   text-transform: uppercase;
@@ -83,8 +89,8 @@ export const Subtitle = styled.h2`
   margin: 8px 0px;
 `
 
-export const FavoriteButton = styled.button<{$isFavorite?: boolean}>`
-  background: ${({$isFavorite}) => $isFavorite ? '#ff9f00' : 'white'};
+export const FavoriteButton = styled.button<{ $isFavorite?: boolean }>`
+  background: ${({ $isFavorite }) => ($isFavorite ? "#ff9f00" : "white")};
   border: none;
   padding: 8px;
   margin: 8px;
--- src/components/character-info/character-info.tsx
+++ src/components/character-info/character-info.tsx
@@ -4,9 +4,10 @@ import Skeleton from "react-loading-skeleton"
 import * as S from "./character-info.styled"
 import { CharacterInfoProps } from "./types"
 import { useBoundStore } from "@/store/store"
+import { BackButton } from "../back-button"
 
 const CharacterInfo: FC<CharacterInfoProps> = ({ isLoading, character }) => {
-  const {favorites, addFavorites, removeFavorite} = useBoundStore()
+  const { favorites, addFavorites, removeFavorite } = useBoundStore()
 
   if (isLoading) {
     return (
@@ -15,7 +16,6 @@ const CharacterInfo: FC<CharacterInfoProps> = ({ isLoading, character }) => {
       </S.Wrapper>
     )
   }
-
 
   const {
     id,
@@ -36,44 +36,73 @@ const CharacterInfo: FC<CharacterInfoProps> = ({ isLoading, character }) => {
     alternateActors,
     alive,
     image,
+    wand,
   } = character
-  const isFavorite = favorites.find(item => item === id)
-  const onFavoriteCallback = () => isFavorite ? removeFavorite(id) : addFavorites(id)
-  const favoriteText = isFavorite ? 'Remove Favorite' : 'Add to favorite'
+  const isFavorite = favorites.find((item) => item === id)
+  const onFavoriteCallback = () =>
+    isFavorite ? removeFavorite(id) : addFavorites(id)
+
+  const favoriteText = isFavorite ? "Remove Favorite" : "Add to favorite"
+
+  const shouldRenderAlternateActors =
+    alternateActors && alternateActors.length > 0
+
+  const wood = wand?.wood
+  const core = wand?.core
+  const length = wand?.length
+
   return (
     <S.Wrapper>
+      <BackButton />
       {image && <S.CharacterImage src={image} />}
       <S.Title>{name}</S.Title>
       <S.Subtitle>{alternateNames?.join(", ")}</S.Subtitle>
       {actor && <S.Text>Portrayed by: {actor}</S.Text>}
 
       <S.FavoriteButton onClick={onFavoriteCallback} $isFavorite={!!isFavorite}>
-       {favoriteText}
+        {favoriteText}
       </S.FavoriteButton>
 
       <S.Information>
         <S.Term>Species </S.Term> <S.Description>{species}</S.Description>
         <S.Term>Gender </S.Term> <S.Description> {gender}</S.Description>
         <S.Term>House </S.Term> <S.Description> {house ?? "N/A"}</S.Description>
-        <S.Term>Birth </S.Term>{" "}
+        <S.Term>Birth </S.Term>
         <S.Description> {dateOfBirth ?? "no information"}</S.Description>
-        <S.Term>Wizard? </S.Term>{" "}
+        <S.Term>Wizard? </S.Term>
         <S.Description>{wizard ? "yes" : "no"}</S.Description>
-        <S.Term>Student? </S.Term>{" "}
+        <S.Term>Student? </S.Term>
         <S.Description>{hogwartsStudent ? "yes" : "no"}</S.Description>
-        <S.Term>Alive? </S.Term>{" "}
+        <S.Term>Alive? </S.Term>
         <S.Description> {alive ? "yes" : "no"}</S.Description>
-        <S.Term>Staff? </S.Term>{" "}
+        <S.Term>Staff? </S.Term>
         <S.Description>{hogwartsStaff ? "yes" : "no"}</S.Description>
-        <S.Term>Ancestry </S.Term>{" "}
+        <S.Term>Ancestry </S.Term>
         <S.Description> {ancestry ?? "no information"}</S.Description>
-        <S.Term>Eye-colour </S.Term>{" "}
+        <S.Term>Eye-colour </S.Term>
         <S.Description>{eyeColour ?? "no information"}</S.Description>
-        <S.Term>Hair-colour </S.Term>{" "}
+        <S.Term>Hair-colour </S.Term>
         <S.Description>{hairColour ?? "no information"}</S.Description>
-        <S.Term>Patronus </S.Term>{" "}
+        <S.Term>Patronus </S.Term>
         <S.Description> {patronus ?? "no information"}</S.Description>
-        {/* {alternateActors?.length && alternateActors.length > 0 && <S.Term>Actors: </S.Term> <S.Description> {alternateActors}</S.Description>} */}
+        {shouldRenderAlternateActors && (
+          <>
+            <S.Term>Actors: </S.Term>
+            <S.Description> {alternateActors.join(", ")}</S.Description>
+          </>
+        )}
+      </S.Information>
+
+      <S.Subtitle>Wand</S.Subtitle>
+      <S.Information $noMarginTop>
+        <S.Term>Wood </S.Term>
+        <S.Description>{wood || "no information"}</S.Description>
+        <S.Term>Core </S.Term>
+        <S.Description>{core || "no information"}</S.Description>
+        <S.Term>Length </S.Term>
+        <S.Description>
+          {length ? `${length} inches` : "no information"}
+        </S.Description>
       </S.Information>
     </S.Wrapper>
   )
