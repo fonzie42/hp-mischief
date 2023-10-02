de4901691a08c2c9a239e61d4f91c0bcc9338e8f
Afonso Ferrer <afonso.ferrer@jackinthebox.com>
2023-10-02 13:55:10 -0300

    [refactor] formatting Character file

--- src/routes/character.tsx
+++ src/routes/character.tsx
@@ -1,23 +1,27 @@
-import {  getCharacterById } from "@/api/base"
+import { getCharacterById } from "@/api/base"
 import { CharacterInfo } from "@/components/character-info"
 import { useBoundStore } from "@/store/store"
 import { useLoaderData } from "react-router-dom"
- import {
-  useQuery,
-} from "@tanstack/react-query"
+import { useQuery } from "@tanstack/react-query"
 import { useEffect } from "react"
 
 export const Character = () => {
   const { characterId } = useLoaderData() //@todo fix type for router
-  const { characters, updateCharacter } = useBoundStore((state) => state)
+  const {characters, updateCharacter} = useBoundStore((state) => state)
 
-  const selectedCharacterFromState = characters.find(item => item.id === characterId)
+  const selectedCharacterFromState = characters.find(
+    (item) => item.id === characterId
+  )
   const enabled = !selectedCharacterFromState
-  
-  const { data, isError, isLoading: isLoadingQuery } = useQuery({
-    queryKey: [characterId], 
+
+  const {
+    data,
+    isError,
+    isLoading: isLoadingQuery,
+  } = useQuery({
+    queryKey: [characterId],
     queryFn: () => getCharacterById(characterId as string),
-    enabled: enabled
+    enabled,
   })
 
   const selectedCharacter = selectedCharacterFromState ?? data // to avoid "not found" false positive
@@ -29,20 +33,16 @@ export const Character = () => {
   const isLoading = isLoadingQuery && enabled
 
   if (isLoading) {
-    console.log('loading')
     return <div>Loading</div>
   }
-  
+
   if (isError) {
     return <div>Sorry, something went wrong</div>
   }
-  
+
   if (!selectedCharacter) {
-    console.log('not found')
     return <div> No files were found. </div>
   }
-  
 
   return <CharacterInfo character={selectedCharacter} />
-
 }
