a7e2f390dfd9a6fe8180fc5329fde81ad02b1188
Afonso Ferrer <afonso.ferrer@jackinthebox.com>
2023-10-02 16:04:03 -0300

    [feat] Basic app functionality for characters

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+ import {
+  useQuery,
+} from "@tanstack/react-query";
+
+import { useEffect } from "react";
+import { getAllCharacters } from "./api/base";
+import { useCharacterStore } from "./store/store";
+import { ListCharacters } from "./routes/list-characters";
+
+
 function App() {
-  const [count, setCount] = useState(0)
+  
+  const { data = [], isError, isLoading } = useQuery(['todos'], () => getAllCharacters())
+
+  const setCharacters = useCharacterStore(state => state.setCharacters)
+
+  useEffect(() => {
+    setCharacters(data)
+  }, [data])
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank" rel="noreferrer">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+      <ListCharacters/>
+  );
 }
 
-export default App
+export default App;
