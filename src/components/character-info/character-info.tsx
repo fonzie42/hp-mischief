b958108e54626ab9ed41b5065ef9e68f4cc9c3c1
Afonso Ferrer <afonso.ferrer@jackinthebox.com>
2023-10-02 13:54:35 -0300

    [feat] Added favorite feature in characters info

--- src/components/character-info/character-info.tsx
+++ src/components/character-info/character-info.tsx
@@ -3,55 +3,80 @@ import { FC } from "react"
 import Skeleton from "react-loading-skeleton"
 import * as S from "./character-info.styled"
 import { CharacterInfoProps } from "./types"
+import { useBoundStore } from "@/store/store"
 
 const CharacterInfo: FC<CharacterInfoProps> = ({ isLoading, character }) => {
+  const {favorites, addFavorites, removeFavorite} = useBoundStore()
+
   if (isLoading) {
-    return <S.Wrapper><Skeleton /></S.Wrapper>
+    return (
+      <S.Wrapper>
+        <Skeleton />
+      </S.Wrapper>
+    )
   }
 
+
   const {
-name
-, alternateNames
-, species
-, gender
-, house
-, dateOfBirth
-, wizard
-, ancestry
-, eyeColour
-, hairColour
-, patronus
-, hogwartsStudent
-, hogwartsStaff
-, actor
-, alternateActors
-, alive
-, image
+    id,
+    name,
+    alternateNames,
+    species,
+    gender,
+    house,
+    dateOfBirth,
+    wizard,
+    ancestry,
+    eyeColour,
+    hairColour,
+    patronus,
+    hogwartsStudent,
+    hogwartsStaff,
+    actor,
+    alternateActors,
+    alive,
+    image,
   } = character
-
-  return <S.Wrapper>
-    {image && <S.CharacterImage src={image} />}
+  const isFavorite = favorites.find(item => item === id)
+  const onFavoriteCallback = () => isFavorite ? removeFavorite(id) : addFavorites(id)
+  const favoriteText = isFavorite ? 'Remove Favorite' : 'Add to favorite'
+  return (
+    <S.Wrapper>
+      {image && <S.CharacterImage src={image} />}
       <S.Title>{name}</S.Title>
-      <S.Subtitle>{alternateNames?.join(', ')}</S.Subtitle>
+      <S.Subtitle>{alternateNames?.join(", ")}</S.Subtitle>
       {actor && <S.Text>Portrayed by: {actor}</S.Text>}
-    
-    <S.Information>
-      <S.Term>Species </S.Term> <S.Description>{species}</S.Description>
-      <S.Term>Gender </S.Term> <S.Description> {gender}</S.Description>
-      <S.Term>House </S.Term> <S.Description> {house ?? 'N/A'}</S.Description>
-      <S.Term>Birth </S.Term> <S.Description> {dateOfBirth ?? 'no information'}</S.Description>
-      <S.Term>Wizard? </S.Term> <S.Description>{wizard ? 'yes' : 'no'}</S.Description>
-      <S.Term>Student? </S.Term> <S.Description>{hogwartsStudent ? 'yes' : 'no'}</S.Description>
-      <S.Term>Alive? </S.Term> <S.Description> {alive ? 'yes' : 'no'}</S.Description>
-      <S.Term>Staff? </S.Term> <S.Description>{hogwartsStaff ? 'yes' : 'no'}</S.Description>
-      <S.Term>Ancestry </S.Term> <S.Description> {ancestry ?? 'no information'}</S.Description>
-      <S.Term>Eye-colour </S.Term> <S.Description>{eyeColour ?? 'no information'}</S.Description>
-      <S.Term>Hair-colour </S.Term> <S.Description>{hairColour ?? 'no information'}</S.Description>
-      <S.Term>Patronus </S.Term> <S.Description> {patronus ?? 'no information'}</S.Description>
-      {/* {alternateActors?.length && alternateActors.length > 0 && <S.Term>Actors: </S.Term> <S.Description> {alternateActors}</S.Description>} */}
 
-    </S.Information>
-  </S.Wrapper>
+      <S.FavoriteButton onClick={onFavoriteCallback} $isFavorite={!!isFavorite}>
+       {favoriteText}
+      </S.FavoriteButton>
+
+      <S.Information>
+        <S.Term>Species </S.Term> <S.Description>{species}</S.Description>
+        <S.Term>Gender </S.Term> <S.Description> {gender}</S.Description>
+        <S.Term>House </S.Term> <S.Description> {house ?? "N/A"}</S.Description>
+        <S.Term>Birth </S.Term>{" "}
+        <S.Description> {dateOfBirth ?? "no information"}</S.Description>
+        <S.Term>Wizard? </S.Term>{" "}
+        <S.Description>{wizard ? "yes" : "no"}</S.Description>
+        <S.Term>Student? </S.Term>{" "}
+        <S.Description>{hogwartsStudent ? "yes" : "no"}</S.Description>
+        <S.Term>Alive? </S.Term>{" "}
+        <S.Description> {alive ? "yes" : "no"}</S.Description>
+        <S.Term>Staff? </S.Term>{" "}
+        <S.Description>{hogwartsStaff ? "yes" : "no"}</S.Description>
+        <S.Term>Ancestry </S.Term>{" "}
+        <S.Description> {ancestry ?? "no information"}</S.Description>
+        <S.Term>Eye-colour </S.Term>{" "}
+        <S.Description>{eyeColour ?? "no information"}</S.Description>
+        <S.Term>Hair-colour </S.Term>{" "}
+        <S.Description>{hairColour ?? "no information"}</S.Description>
+        <S.Term>Patronus </S.Term>{" "}
+        <S.Description> {patronus ?? "no information"}</S.Description>
+        {/* {alternateActors?.length && alternateActors.length > 0 && <S.Term>Actors: </S.Term> <S.Description> {alternateActors}</S.Description>} */}
+      </S.Information>
+    </S.Wrapper>
+  )
 }
 
 export default CharacterInfo
